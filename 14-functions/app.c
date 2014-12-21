a235923e1022c1e8138943e1bb724e31c7cff863
Jesse Shawl <jesse@jshawl.com>
2014-12-21 15:04:33 -0500

    do the extra credit

--- 14-functions/app.c
+++ 14-functions/app.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int can_print_it( char ch );
 void print_letters( char arg[] );
@@ -13,17 +14,13 @@ void print_arguments( int argc, char *argv[] ){
 
 void print_letters( char arg[] ){
   int i = 0;
-  for( i = 0; arg[i] != '\0'; i++ ){
+  for( i = 0; i < strlen(arg) ; i++ ){
     char ch = arg[i];
-    if( can_print_it( ch ) ){
+    if( isalpha( ch ) || isblank( ch ) ){
       printf("'%c'== %d", ch, ch);
     }
   }
   printf("\n");
-}
-
-int can_print_it( char ch ){
-  return isalpha( ch ) || isblank( ch );
 }
 
 int main( int argc, char *argv[] ) {
